6f174f718a49f00961561f6147c16d0424e6db90
myracode <83231015+myracode@users.noreply.github.com>
2023-02-12 03:35:53 -0500

    Update the Wedscraper

--- Webscraper.py
+++ Webscraper.py
@@ -26,24 +26,14 @@ def html_code(url):
 url = "https://www.amazon.ca/Apple-MLWK3AM-A-New-AirPods/product-reviews/B09JQMJHXY/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"
   
 soup = html_code(url)
-#print(soup)
 
-    
+alist = list()
+
 def cus_rev(soup):
-    # find the Html tag
-    # with find()
-    # and convert into string
     data_str = ""
-    alist = list()
-    #data_str_1 = ""
   
     for items in soup.find_all("div", class_="a-row a-spacing-small review-data"):
-        #for item in soup.find_all("div", id_='customer_review-R5ZLCO12H50ZL', class_='a-row a-spacing-small review-data'):
-           # data_str_1 = data_str_1 + item.get_text()
-        #data_str = data_str + items.get_text()
         alist.append(items)
-       # result_1 = data_str_1.split("\n")
-    #result = data_str.split("\n")
     print(len(alist))
     return(alist)
 
@@ -55,15 +45,13 @@ for i in rev_data:
     else:
         rev_result.append(i)
 rev_result
-#print(rev_result)
 
-#headline = div.h2.text
-#print(headline)
+for x in range(2,10):
+    soup = html_code(f'https://www.amazon.ca/Apple-MLWK3AM-A-New-AirPods/product-reviews/B09JQMJHXY/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber={x}')
+    print(f'Getting page: {x}')
+    cus_rev(soup)    
+    print(len(alist))
 
-#subparagraph = div.find('header', class_='major').p.text
-#print(subparagraph)
 
-#summary = article.find('div', class_='ml-3').p.text
-#print(summary)
-
+    
 
